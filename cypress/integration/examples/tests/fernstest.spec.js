6048ecd0ec3ee971bad1996628b8773460766a6a
Fernando Chavez <fjchavez01@gmail.com>
2020-10-15 13:39:25 -0700

    added checkbox test

--- cypress/integration/examples/tests/fernstest.spec.js
+++ cypress/integration/examples/tests/fernstest.spec.js
@@ -10,25 +10,35 @@ describe('Form App', () => {
     const nameInput = () => cy.get('input[name="name"]')
     const emailInput = () => cy.get('input[name="email"]')
     const passInput = () => cy.get('input[name="password"]')
+    const checkInput = () => cy.get('input[name="tos"]')
     
-    it('Tests name input', () => {
+    const nameTest = () => {it('Tests name input', () => {
         nameInput().should('have.value', '')
         nameInput().type('Example Name')
         nameInput().should('have.value', 'Example Name')
-    })
+    })}
+    nameTest()
 
-    it('Tests email input', () => {
+    const emailTest = () => {it('Tests email input', () => {
         emailInput().should('have.value', '')
         emailInput().type('Email@example.com')
         emailInput().should('have.value', 'Email@example.com')
-    })
+    })}
+    emailTest()
 
-    it('Tests password input', () => {
+    const passwordTest = () => {it('Tests password input', () => {
         passInput().should('have.value', '')
         passInput().type('Password')
         passInput().should('have.value', 'Password')
-    })
+    })}
+    passwordTest()
 
+    const checkTest = () => {it('Tests tos input', () => {
+        checkInput().should('have.value', 'false')
+        checkInput().click()
+        checkInput().should('have.value', 'true')
+    })}
+    checkTest()
 
 
 
